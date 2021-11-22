1a4336cfe844796b245000f9d3e6fc00980a768d
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2021-11-22 00:35:14 -0600

    ghjgf

--- src/net/pgfmc/teams/inventory/FriendsInventory.java
+++ src/net/pgfmc/teams/inventory/FriendsInventory.java
@@ -2,19 +2,62 @@ package net.pgfmc.teams.inventory;
 
 import org.bukkit.Material;
 
+import net.pgfmc.pgfessentials.inventoryAPI.InteractableInventory;
 import net.pgfmc.pgfessentials.inventoryAPI.PagedInventory;
 import net.pgfmc.pgfessentials.playerdataAPI.PlayerData;
 import net.pgfmc.teams.friends.Friends;
 
-public class FriendsInventory extends PagedInventory<PlayerData> {
-
-	public FriendsInventory(PlayerData player) {
-		super(SizeData.SMALL, "Friends List", Friends.getFriendsList(player.getUniqueId()), (x) -> {
-			return createButton(Material.PAPER, "§o" + x.getRankedName(), null, (p, e) -> {
-				
-				// insert new inventory here
-				
+public class FriendsInventory {
+	
+	/**
+	 * Friends List inventory
+	 * Opens the player's friends list
+	 * @author CrimsonDart
+	 *
+	 */
+	public static class FriendsList extends PagedInventory<PlayerData> {
+		
+		public FriendsList(PlayerData player) {
+			super(SizeData.SMALL, "Friends List", Friends.getFriendsList(player.getUniqueId()), (x) -> {
+				return createButton(Material.PAPER, "§o" + x.getRankedName(), null, (p, e) -> {
+					
+					p.openInventory(new FriendOptions(player, x).getInventory());
+					
+				});
 			});
-		});
+		}
 	}
+	
+	/**
+	 * Friend Options inventory
+	 * allows Unfriend, Favorite (or Unfavorite), block, report ..../????
+	 * 
+	 * 
+	 * @author james
+	 *
+	 */
+	public static class FriendOptions extends InteractableInventory {
+
+		public FriendOptions(PlayerData player, PlayerData friend) {
+			super(27, "Options for §o" + friend.getRankedName());
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
 }
