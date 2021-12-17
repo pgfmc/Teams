ee87e0e98d454effb7c937cc6331f0709a6ea06c
bk <33919755+bkiscool@users.noreply.github.com>
2021-12-16 23:40:34 -0800

    Add files via upload

---
+++ src/net/pgfmc/teams/friends/FriendAcceptCommand.java
@@ -0,0 +1,22 @@
+package net.pgfmc.teams.friends;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FriendAcceptCommand implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if (!(sender instanceof Player))
+		{
+			sender.sendMessage("Data output to the console.");
+		}
+		
+		Friends.DEFAULT.accept((Player) sender);
+		
+		return true;
+	}
+}
---
+++ src/net/pgfmc/teams/friends/FriendsListCommand.java
@@ -0,0 +1,26 @@
+package net.pgfmc.teams.friends;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.pgfmc.core.playerdataAPI.PlayerData;
+
+public class FriendsListCommand implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (!(sender instanceof Player)) {
+			return true;
+		}
+		
+		for (PlayerData pd : Friends.getFriendsMap(PlayerData.getPlayerData((Player) sender)).keySet()) {
+			sender.sendMessage("§n" + pd.getRankedName());
+		}
+		
+		return true;
+	}
+
+}
